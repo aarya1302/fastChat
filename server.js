b2b9ee9b64b060fdca712ef1f35dea61ca3b0e32
System Administrator <root@aaryas-iMac.local>
2021-12-01 17:52:44 +0400

    fixing bugs

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ const { ObjectId } = require("bson");
 //importing socket.io listeners
 const chat_listeners = require("./chat-listeners");
 
+
 //general middlewares
 app.use(cors());
 app.use(bodyParser());
@@ -95,6 +96,8 @@ chat_listeners(io, db);
 app.set("view engine", "pug");
 
 app.get("/", ensureAuthenticated, (req, res) => {
+  cons
+  console.log(process.cwd())
   res.sendFile(process.cwd()+"/chat-app/build/index.html");
 });
 
