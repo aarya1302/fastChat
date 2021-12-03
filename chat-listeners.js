feed825dfb61b92fe1a4ff823e490121ddaff384
aarya <aarya@aaryas-MacBook-Pro.local>
2021-12-04 01:32:28 +0400

    allowing chat with offline users

--- chat-listeners.js
+++ chat-listeners.js
@@ -64,12 +64,16 @@ module.exports = (io, db) => {
           }
         );
       });
-      socket
+      var userOnline = OnlineUsersStore[username]
+      if(userOnline){
+        socket
         .to(OnlineUsersStore[username]["socketId"])
         .emit("receive message", {
           from: socket_user.username,
           message: { text: msg, self: false },
         });
+      }
+
     });
   });
 };
